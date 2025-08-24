5c8c95490c09d0c24c2c2937a25fa7316003e8ff
thientn4 <ntmthien2001@gmail.com>
2025-08-24 09:13:02 -0700

    update project info

---
+++ codebase/src/App.js
@@ -0,0 +1,202 @@
+import './App.css';
+import {useState,useEffect,useRef} from 'react'
+import data from './data.js'
+
+function useIsVisible(ref) {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) =>
+      setIntersecting(entry.isIntersecting)
+    );
+
+    observer.observe(ref.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  return isIntersecting;
+}
+
+function getYOE() {
+  const now = new Date();
+  const march2023 = new Date('2022-03-01T00:00:00Z');
+  const differenceInMilliseconds = now - march2023;
+
+  // Convert milliseconds to years
+  // 1 year = 365.25 days to account for leap years
+  const millisecondsInYear = 1000 * 60 * 60 * 24 * 365.25;
+  const years = differenceInMilliseconds / millisecondsInYear;
+
+  return years.toFixed(1);
+}
+
+function App() {
+  const aboutRef = useRef();
+  const skillsRef = useRef();
+  const projectsRef = useRef();
+  const aboutVisible = useIsVisible(aboutRef)
+  const skillsVisible = useIsVisible(skillsRef)
+  const projectsVisible = useIsVisible(projectsRef)
+  const [skillType,setSkillType]=useState(1)
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+
+    return () => { window.removeEventListener('resize', handleResize); };
+  }, []);
+  const styles={
+    screen:{
+      minHeight:'100svh',
+      display:'flex',
+      flexDirection:windowWidth<700?'column-reverse':'row',
+      backgroundColor:'white'
+    },
+    navBar:{
+      display:'flex',
+      flexDirection:windowWidth<700?'row':'column',
+      justifyContent:'space-around',
+      borderRight:windowWidth<700?'none':'solid 0.01in grey',
+      borderTop:windowWidth>=700?'none':'solid 0.01in grey',
+      position:'fixed',
+      left:0,
+      height:windowWidth>=700?'100svh':'fit-content',
+      width:windowWidth<700?'100svw':'fit-content',
+      backgroundColor:'white'
+    },
+    content:{
+      display:'flex',
+      flexDirection:'column',
+      flex:1,
+      alignItems:'center',
+      overflowX:'auto',
+      paddingLeft:windowWidth>=700?'0.55in':0,
+      paddingTop:windowWidth<700?'0.55in':0,
+      paddingBottom:windowWidth<700?'0.55in':0
+    },
+    button:{
+      width:'0.35in',
+      aspectRatio:1,
+      margin:'0.1in',
+      borderRadius:'100%'
+    },
+    section:{
+      display:'flex',
+      flexDirection:'column',
+      alignItems:'center',
+      textAlign:'center',
+      maxWidth:'7in',
+      padding:'0.1in',
+      minHeight:windowWidth>=700?'100svh':'calc(100svh - 1.1in)'
+    },
+    yellowHighlight:{
+      fontWeight:'bold',
+      background:"linear-gradient(to top, #FFEC84 50%, white 50%)",
+      whiteSpace:'nowrap',
+      width:'fit-content'
+    },
+    blueHighlight:{
+      fontWeight:'bold',
+      background:"linear-gradient(to top, #cbe6ef 50%, white 50%)",
+      whiteSpace:'nowrap',
+      width:'fit-content'
+    },
+    skillBar:{
+      width:'100%',
+      marginTop:'0.4in',
+      paddingBottom:'0.1in',
+      display:'flex',
+      flexDirection:'row',
+      color:'grey',
+      justifyContent:'space-around',
+      borderBottom:'solid 0.01in grey'
+    }
+  }
+  return (
+    <div style={styles.screen}>
+      <div style={styles.content}>
+        <div id="about_me" style={styles.section} ref={aboutRef}>
+          <div style={{flex:1}}/>
+          <img alt="about me" style={{width:'70%',maxWidth:'4in',marginTop:'0.5in'}} src={require("./image/intro.jpg")}/>
+          <div style={{marginTop:'0.2in',color:'grey',fontWeight:'bold',fontSize:'0.3in'}}>Hello from Winston!</div>
+          <div style={{marginTop:'0.2in',fontSize:'0.2in'}}>
+            <span style={styles.blueHighlight}>Full Stack Software Engineer</span> with {getYOE()} years of experience transforming <span style={styles.yellowHighlight}>business needs</span> into powerful, user-centric <span style={styles.yellowHighlight}>software solutions</span>
+          </div>
+          <div style={{marginTop:'0.2in',display:'flex',flexDirection:'row',marginBottom:'0.5in'}}>
+            <img alt="email" style={styles.button} src={require("./image/buttons/email.png")} onClick={()=>window.location.href="mailto:ntmthien2001@gmail.com"}/>
+            <img alt="github" style={styles.button} src={require("./image/buttons/github.png")} onClick={()=>window.open('https://github.com/thientn4', '_blank')}/>
+            <img alt="linkedin" style={styles.button} src={require("./image/buttons/linkedin.png")} onClick={()=>window.open('https://www.linkedin.com/in/thiennguyen2001/', '_blank')}/>
+            <img alt="resume" style={styles.button} src={require("./image/buttons/resume.png")} onClick={()=>window.open('https://docs.google.com/document/d/1QMwdUgDgUzbeAUTa0FqKTwcwAzP3RTydyg0EzU-hzOA/edit?usp=sharing', '_blank')}/>
+          </div>
+          <div style={{flex:1}}/>
+        </div>
+        <div style={styles.section}>
+          <div style={{flex:1}}/>
+          <div ref={skillsRef} id="my_skills" style={{marginTop:'1in',paddingTop:windowWidth<700?'0.55in':0}}>
+            <div style={{fontSize:'0.2in', fontWeight:'bold',paddingTop:'0.3in'}}>
+              <span style={styles.blueHighlight}>Full Stack</span>? What do I <span style={styles.yellowHighlight}>use</span>?
+            </div>
+            {windowWidth>=700 && <div style={styles.skillBar}>
+              <div style={{color:skillType===0?'black':'grey', textDecoration:skillType===0?'underline':'none', userSelect:'none'}} onClick={()=>{setSkillType(0)}}>Data</div>
+              <div style={{color:skillType===1?'black':'grey', textDecoration:skillType===1?'underline':'none', userSelect:'none'}} onClick={()=>{setSkillType(1)}}>Backend</div>
+              <div style={{color:skillType===2?'black':'grey', textDecoration:skillType===2?'underline':'none', userSelect:'none'}} onClick={()=>{setSkillType(2)}}>Frontend</div>
+              <div style={{color:skillType===3?'black':'grey', textDecoration:skillType===3?'underline':'none', userSelect:'none'}} onClick={()=>{setSkillType(3)}}>Tool</div>
+            </div>}
+            {windowWidth<700 && <select style={{...styles.skillBar,border:'solid 0.01in grey',color:'grey',textAlign:'center',outline: 'none',fontSize:15,backgroundColor:'white',padding:'0.05in',borderRadius:'0.1in'}} value={skillType} onChange={(e)=>{setSkillType(parseInt(e.target.value))}}>
+              <option value={0}>Data</option>
+              <option value={1}>Backend</option>
+              <option value={2}>Frontend</option>
+              <option value={3}>Tool</option>
+            </select>}
+            <div style={{display:'flex',flexDirection:'row',flexWrap:'wrap',justifyContent:'space-evenly'}}>
+              {data.skills.map((skill,index)=><div style={{margin:'0.15in',flex:1,opacity:skillType===skill.type?1:0.25,backgroundColor:'white'}}  onClick={()=>{setSkillType(skill.type)}}>
+                <img alt="skill" style={{height:'0.5in',margin:'0.1in',filter:skillType===skill.type?'grayscale(0%)':'grayscale(100%)'}} src={skill.logo}/>
+                <div style={{whiteSpace:'pre-wrap',userSelect:'none'}}>{skill.name}</div>
+              </div>)}
+            </div>
+          </div>
+          <div style={{flex:1}}/>
+        </div>
+        <div style={{...styles.section,maxWidth:'8in'}}>
+          <div style={{flex:1}}/>
+          <div ref={projectsRef} id="my_projects" style={{marginTop:'1in',paddingTop:windowWidth<700?'0.55in':0}}>
+            <div style={{fontSize:'0.2in', fontWeight:'bold',paddingTop:'0.3in'}}>
+              I build for <span style={styles.yellowHighlight}>businesses</span> and for <span style={styles.blueHighlight}>fun</span>!
+            </div>
+            <div style={{display:'flex',flexDirection:'row',flexWrap:'wrap',justifyContent:'center',marginTop:'0.3in'}}>
+              {data.projects.map((project,index)=><div style={{margin:'0.1in',padding:'0.15in',maxWidth:'2in', border:'solid 0.01in grey',borderRadius:'0.1in',alignItems:'center',display:'flex',flexDirection:'column'}}>
+                <img alt="project" style={{height:'0.5in',marginBottom:'0.1in'}} src={project.logo || require("./image/projects/project.png")}/>
+                <div style={{...styles[project.business?'yellowHighlight':'blueHighlight'],marginBottom:'0.1in'}}>{project.name}</div>
+                <div>{project.about}</div>
+                <div style={{flex:1}}/>
+                <u style={{color:'grey',userSelect:'none',marginTop:'0.1in'}} onClick={()=>{
+                  if(project.link) 
+                    window.open(project.link, '_blank') 
+                  else 
+                    alert("Coming Soon!")
+                }}>more</u>
+              </div>)}
+            </div>
+          </div>
+          <div style={{flex:1}}/>
+        </div>
+      </div>
+      <div style={{...styles.navBar,bottom:0}}>
+        {windowWidth>=700 && <img  alt='Winston' style={{...styles.button, borderRadius:0}} src={require("./image/logo.png")}/>}
+        {windowWidth>=700 && <div style={{flex:1}}/>}
+        <img alt="about" style={{...styles.button,opacity:(skillsVisible || projectsVisible)?0.5:1}} src={require("./image/buttons/about.png")} onClick={()=>window.scrollTo({behavior: 'smooth', top: 0})}/>
+        <img alt="skills" style={{...styles.button,opacity:(skillsVisible)?1:0.5}} src={require("./image/buttons/skills.png")} onClick={()=>document.getElementById('my_skills').scrollIntoView({behavior: 'smooth', block: 'start'})}/>
+        <img alt="projects" style={{...styles.button,opacity:(aboutVisible || skillsVisible)?0.5:1}} src={require("./image/buttons/projects.png")} onClick={()=>document.getElementById('my_projects').scrollIntoView({behavior: 'smooth', block: 'start'})}/>
+      </div>
+      {windowWidth<700 && <div style={{...styles.navBar,border:'none',top:0}}>
+        <img alt="Winston" style={{...styles.button, borderRadius:0}} src={require("./image/logo.png")}/>
+      </div>}
+    </div>
+  );
+}
+
+export default App;
---
+++ codebase/src/data.js
@@ -0,0 +1,123 @@
+const data={
+    skills:[
+        {
+            "name":"Microsoft\nSQL",
+            "type":0,
+            "logo":require("./image/skills/microsoftSQL.png")
+        },
+        {
+            "name":"MySQL",
+            "type":0,
+            "logo":require("./image/skills/mySQL.jpg")
+        },
+        {
+            "name":"PostgreSQL",
+            "type":0,
+            "logo":require("./image/skills/postgreSQL.png")
+        },
+        {
+            "name":"IBM-DB2",
+            "type":0,
+            "logo":require("./image/skills/ibmDB2.webp")
+        },
+        {
+            "name":"ASP.NET\n(C#)",
+            "type":1,
+            "logo":require("./image/skills/asp.png")
+        },
+        {
+            "name":"Python",
+            "type":1,
+            "logo":require("./image/skills/python.png")
+        },
+        {
+            "name":"NodeJS\n(Javascript)",
+            "type":1,
+            "logo":require("./image/skills/node.webp")
+        },
+        {
+            "name":"React\n(JS-Native)",
+            "type":2,
+            "logo":require("./image/skills/react.png")
+        },
+        {
+            "name":"Github",
+            "type":3,
+            "logo":require("./image/skills/github.png")
+        },
+        {
+            "name":"AWS",
+            "type":3,
+            "logo":require("./image/skills/aws.png")
+        },
+        {
+            "name":"Expo",
+            "type":3,
+            "logo":require("./image/skills/expo.png")
+        },
+        {
+            "name":"Postman",
+            "type":3,
+            "logo":require("./image/skills/postman.png")
+        }
+    ],
+    projects:[
+        // {
+        //     "name":"Ascending Eagle",
+        //     "business":true,
+        //     "logo":require("./image/projects/ascendingEagle.png"),
+        //     "about":"This is a project. This is a project. This is a project. This is a project. This is a project. This is a project. ",
+        //     "link":null
+        // },
+        {
+            "name":"JMEagle",
+            "business":true,
+            "logo":require("./image/projects/jmEagle.png"),
+            "about":"Full-stack web and mobile platform for JMEagle that streamlines delivery management and provides real-time driver tracking.",
+            "link":"https://docs.google.com/document/d/1l5KPTR-ybhaS_aAdGirfmXZ-kV4M1rJSL-TwLJ62dTA/edit"
+        },
+        {
+            "name":"Breadwinner",
+            "business":false,
+            "logo":require("./image/projects/breadwinner.png"),
+            "about":"Mobile app to turn private recipes into seamless meal and grocery plans, simplify mealprep and grocery for busy family",
+            "link":"https://apps.apple.com/gb/app/breadwinner-meal-prep/id6749663952?uo=2"
+        },
+        {
+            "name":"iMock",
+            "business":false,
+            "logo":require("./image/projects/iMock.png"),
+            "about":"Web app to give job seekers a competitive edge with AI mock interviews, delivering instant feedback from Google Gemini AI",
+            "link":"https://www.youtube.com/watch?v=79piTHfBSns"
+        },
+        {
+            "name":"unBlock",
+            "business":false,
+            "logo":require("./image/projects/unBlock.png"),
+            "about":"Web app for teachers to create engaging virtual classrooms where students can ask, discuss and answer questions",
+            "link":"https://docs.google.com/document/d/1pcJwgOwITQdarScoO7_UDVZ387enB9Rl_2EPkLzMeI0/edit?tab=t.0"
+        },
+        {
+            "name":"MeetLink",
+            "business":false,
+            "logo":require("./image/projects/meetLink.png"),
+            "about":"Web app to streamline team scheduling, helping groups find optimal time for meetings and maximize attendance",
+            "link":"https://github.com/thientn4/MeetLink"
+        },
+        {
+            "name":"SortVisual",
+            "business":false,
+            "logo":null,
+            "about":"A website to visualise common types of sort algorithm (selection, bubble, insertion, quick, merge, heap)",
+            "link":"https://thientn4.github.io/sort_visualizer/"
+        },
+        {
+            "name":"TicTacToeAI",
+            "business":false,
+            "logo":null,
+            "about":"Unbeatable Tic Tac Toe AI that leverages decision-tree algorithm to calculate win/lose probabilities, guaranteeing victory",
+            "link":"https://thientn4.github.io/Tic-Tac-Toe/"
+        }
+    ]
+}
+export default data
